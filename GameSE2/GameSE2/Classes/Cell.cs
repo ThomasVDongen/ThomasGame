97b56172046ef1fb560c7441294d54dbe55e0dcb
Dongen, Thomas T.G.T.A. van <tgta.vandongen@student.fontys.nl>
2016-03-11 11:40:51 +0100

    Initiatie van de klassen

---
+++ GameSE2/GameSE2/Classes/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSE2.Classes
+{
+    public class Cell
+    {
+       
+    }
+
+    public enum CellType
+    {
+        Normal, Goal, Wall
+    }
+}
---
+++ GameSE2/GameSE2/Classes/Map.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameSE2.Classes
+{
+    public class Map
+    {
+        public Size MapSize { get; set; }
+    }
+}
